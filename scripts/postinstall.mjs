a2362b522c639e059af9c57070f790ff9af3b15f
Atolycs <111569596+atolycs@users.noreply.github.com>
2026-03-27 10:16:21 +0900

    feat: add autoinstall

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -1,10 +1,19 @@
 import * as fs from "fs"
-import * as path from "path"
+import { resolve, dirname, join } from "path"
+import { execSync } from "child_process"
+import { fileURLToPath } from "url";
 
 const basePath = process.env.INIT_CWD || process.cwd();
-const biomeConfigPath = path.join(basePath, "biome.json")
-const lefthookPath = path.join(basePath, "lefthook.json")
+const biomeConfigPath = join(basePath, "biome.json")
+const lefthookPath = join(basePath, "lefthook.json")
 
+const userAgent = process.env.npm_config_user_agent ?? '';
+const isPnpm = userAgent.startsWith('pnpm')
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const packageJson = JSON.parse(readFileSync(resolve(__dirname, '../package.json'), 'utf-8'))
+const packages = Object.keys(packageJson.dependencies ?? {});
 
 const biome_config_map = {
   "$schema": "./node_modules/@biomejs/biome/configuration_schema.json",
@@ -33,3 +42,9 @@ if (!fs.existsSync(lefthookPath)) {
   fs.writeFileSync(lefthookPath, JSON.stringify(lefthook_config_map, null, 2))
 }
 
+if (!isPnpm) process.exit(0)
+
+for (const pkg of packages) {
+  console.log(`📦 Hoisting ${pkg} to project root ...`)
+  execSync(`pnpm add ${pkg}`, { cwd: basePath, stdio: 'inherit' })
+}
